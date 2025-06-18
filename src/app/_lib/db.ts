96189c16d31cd018f19afd33a16a0c873d899627
Linh <nguyenthilinhqni2020@gmail.com>
2025-06-18 13:14:14 +0700

    connect with aiven

--- src/app/_lib/db.ts
+++ src/app/_lib/db.ts
@@ -1,12 +1,16 @@
 import fs from 'fs';
+import path from 'path';
 import mysql from 'mysql2/promise';
-const ca = fs.readFileSync('public/ca.pem');
+
+// Đường dẫn chính xác đến file ca.pem
+const ca = fs.readFileSync(path.join(process.cwd(), 'src','app', 'certs', 'ca.pem'));
+
 export const db = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: Number(process.env.DB_PORT), // nhớ convert sang số nếu dùng env
+  port: Number(process.env.DB_PORT),
   ssl: {
     ca,
   },
